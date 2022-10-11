035b22e5bfaf109453473421265a8e11826d6b40
Mike Kurkin <mike@kurkin.net>
2022-10-11 23:32:16 +0300

    associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :my_tasks, class_name: 'Task',
+                      inverse_of: :author,
+                      dependent: :destroy
+  has_many :assigned_tasks, class_name: 'Task',
+                            inverse_of: :assignee,
+                            dependent: :nullify
 end
