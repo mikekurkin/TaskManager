4d8bb4a62f3e33e3e47c95010675c6119bf010aa
Mike Kurkin <mike@kurkin.net>
2022-10-11 23:25:03 +0300

    model tests

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
-    name { 'MyString' }
-    description { 'MyText' }
-    author { nil }
-    assignee { nil }
+    name
+    description
+    author factory: :manager
+    assignee factory: :developer
     state { 'MyString' }
     expired_at { '2022-10-11' }
   end
