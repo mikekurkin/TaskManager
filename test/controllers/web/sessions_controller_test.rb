4340c713802ef4aeb6aedd8b853fd85af02e00c3
Mike Kurkin <mike@kurkin.net>
2022-10-13 13:18:57 +0000

    controller tests

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
-class Web::SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get web_sessions_new_url
+class Web::SessionsControllerTest < ActionController::TestCase
+  test 'should get new' do
+    get :new
     assert_response :success
   end
 end
