aabe27767052950b9a8e63847ca52e993a0b485b
Mike Kurkin <mike@kurkin.net>
2022-10-17 18:58:10 +0000

    use sequences for all factory fields

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
-  sequence :string,
-           aliases: [:first_name, :last_name, :password,
-                     :name, :description] do |n|
-    "string#{n}"
-  end
+  sequence(:first_name) { |n| "FirstName#{n}" }
+  sequence(:last_name) { |n| "LastName#{n}" }
+  sequence(:email) { |n| "email#{n}@example.com" }
+  sequence(:password) { |n| "pa$sW0rd#{n}!" }
+  sequence(:avatar) { |n| "https://randomuser.me/api/portraits/thumb/men/#{n % 100}.jpg" }
+  sequence(:type) { [Developer, Manager].sample }
 
-  sequence :email do |n|
-    "email#{n}@example.com"
-  end
+  sequence(:name) { |n| "Task ##{n}" }
+  sequence(:description) { |n| "Task Description #{n}" }
+  sequence(:expired_at) { (Date.current + rand(-30..30).days).strftime('%Y-%m-%d') }
 end
