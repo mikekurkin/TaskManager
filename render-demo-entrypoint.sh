33d1f9c6c63f78503c183ae7d2020c8880a4150a
Mike Kurkin <mike@kurkin.net>
2022-11-07 14:02:18 +0000

    make seed replant only if environment variable set

--- render-demo-entrypoint.sh
+++ render-demo-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 bundle exec rails db:prepare
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 \
-bundle exec rails db:seed:replant
+if [ ! -z "$REPLANT_SEED" ]; then
+    DISABLE_DATABASE_ENVIRONMENT_CHECK=1 \
+    bundle exec rails db:seed:replant
+    export REPLANT_SEED=''
+fi
 bundle exec rails server
